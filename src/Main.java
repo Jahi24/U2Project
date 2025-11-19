7d1d58436f272149bbe5f95e2e19afb6759a62ce
JahiDesormeaux-will <jahisaleem@gmail.com>
2025-11-19 08:45:41 -0500

    Made main into a RPS method

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Let's play rock, paper, scissors!");
         Roshambo.unfairGame();
+        System.out.println();
+        System.out.println("Did you want to play fair?");
+        String request = scan.nextLine();
+        if (Roshambo.yesCheck(request)) {
+            Roshambo.fairGame();
+        }
+        System.out.println("Goodbye! I had fun!");
     }
 }
--- src/Roshambo.java
+++ src/Roshambo.java
@@ -1,63 +1,116 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Roshambo {
-    public static boolean rockCheck(String pick){
+    public static boolean rockCheck(String pick) {
         String small = pick.toLowerCase();
         return small.indexOf("r") == 0;
     }
 
-    public static boolean paperCheck(String pick){
+    public static boolean paperCheck(String pick) {
         String small = pick.toLowerCase();
         return small.indexOf("p") == 0;
     }
 
-    public static boolean scissorsCheck(String pick){
+    public static boolean scissorsCheck(String pick) {
         String small = pick.toLowerCase();
         return small.indexOf("s") == 0;
     }
 
-    public static String check(String pick){
+    public static String check(String pick) {
         String result = "";
-        if (rockCheck(pick)){
+        if (rockCheck(pick)) {
             result = "rock";
         }
-        if (paperCheck(pick)){
+        if (paperCheck(pick)) {
             result = "paper";
         }
-        if (scissorsCheck(pick)){
+        if (scissorsCheck(pick)) {
             result = "scissors";
         }
         return result;
     }
-    public static String cheat(String ans){
+
+    public static boolean yesCheck(String pick){
+        String small = pick.toLowerCase();
+        return small.indexOf("y") == 0;
+    }
+
+    public static String cheat(String ans) {
         String result = "";
-        if (ans == "rock"){
+        if (rockCheck(ans)) {
             result = "paper";
+            return result;
         }
-        if (ans == "paper"){
+        if (paperCheck(ans)) {
             result = "scissors";
+            return result;
         }
-        if (ans == "scissors"){
+        if (scissorsCheck(ans)) {
             result = "rock";
+            return result;
         }
         return result;
     }
-    public static void unfairGame(){
+
+    public static String computerPick(int input){
+        String result = "";
+        if (input == 1) {
+            result = "paper";
+            return result;
+        }
+        if (input == 2) {
+            result = "scissors";
+            return result;
+        }
+        if (input == 3) {
+            result = "rock";
+            return result;
+        }
+        return result;
+    }
+
+    public static void unfairGame() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Your Move: ");
         String move = scan.nextLine();
         String moveReal = Roshambo.check(move);
+        if (Objects.equals(moveReal, "")) {
+            return;
+        }
         String cheatMove = Roshambo.cheat(moveReal);
         System.out.println("I said..." + cheatMove);
         System.out.println(cheatMove + " beats " + moveReal);
         System.out.println("I win! Let's go again!");
-        while (Roshambo.rockCheck(moveReal) || Roshambo.paperCheck(moveReal) || Roshambo.scissorsCheck(moveReal)) {
+        while (!moveReal.equals("")) {
             System.out.print("(Your Move): ");
             move = scan.nextLine();
             moveReal = Roshambo.check(move);
+            if (Objects.equals(moveReal, "")) {
+                break;
+            }
+            cheatMove = Roshambo.cheat(move);
             System.out.println("I said..." + cheatMove);
             System.out.println(cheatMove + " beats " + moveReal);
             System.out.println("I win! Let's go again!");
+        }
+        System.out.println("What?");
+    }
+
+    public static void fairGame() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Best 2 out of 3? or \nBest 3 out of 5?");
+        System.out.print("Type \"1\" for the first option and \"2\" for the second option.");
+        int selection = scan.nextInt();
+        if (selection == 1){ // 2:3 RPS game
+            int myScore = 0;
+            int yourScore = 0;
+            String myPick = Roshambo.computerPick((int)((Math.random() * 2) + 1));
+            System.out.print("Your Move: ");
+            String move = scan.nextLine();
+            String yourPick = Roshambo.check(move);
+        } else if (selection == 2){ // 3:5 RPS game
+
         }
     }
 }
